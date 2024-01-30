5cb531722b8437d3553e3c16af25ca840703074e
Zeus <elderfieldzeus24@gmail.com>
2024-01-30 20:13:41 +0800

    added files to todoList repo

---
+++ script.js
@@ -0,0 +1,39 @@
+const taskbar = document.getElementById("taskbar");
+const button = document.getElementById("button");
+const container = document.querySelector(".task-collection");
+
+button.addEventListener("click", () => addTask());
+taskbar.addEventListener("keydown", (event) => {
+    if(event.key === "Enter") addTask();
+});
+
+const randomTask = () => {
+    const tasks = ["Ride a bike...", "Cook food...", "Do homework...", "Study for exam...", "Code...", "Take out the trash..."];
+    const index = Math.floor(Math.random() * tasks.length);
+    return tasks[index];
+}
+
+taskbar.placeholder = randomTask();
+
+const addTask = () => {
+    if(taskbar.value === "") {
+        alert("TASKBAR CANNOT BE EMPTY"); 
+        return;
+    }
+
+    const taskDiv = document.createElement("div");
+    const task = document.createElement("h3");
+    const removeButton = document.createElement("button");
+
+    removeButton.textContent = "DONE";
+    removeButton.addEventListener("click", () => removeTask(taskDiv));
+    task.textContent = taskbar.value;
+    taskDiv.appendChild(task);
+    taskDiv.appendChild(removeButton);
+    container.prepend(taskDiv);
+
+    taskbar.value = "";
+    taskbar.placeholder = randomTask();
+}
+
+const removeTask = (taskDiv) => container.removeChild(taskDiv);
